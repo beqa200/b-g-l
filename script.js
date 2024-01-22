01bcdc1450c6945190b602efc92cb94ae935be3f
lasha <gnomi490@gmail.com>
2024-01-22 23:09:38 +0400

    add register form

---
+++ script.js
@@ -0,0 +1,47 @@
+const income=document.querySelector('.income');
+const cross=document.querySelector('.cross')
+const register=document.querySelector('.register')
+const modalEmail=document.querySelector('.modal__email')
+const logIn=modalEmail.value
+
+if(localStorage.getItem('email')){
+  modalEmail.value=localStorage.getItem('email')
+}
+
+
+
+
+cross.addEventListener('click',()=>{
+    document.querySelector('#modalWindow').style.visibility='hidden'
+})
+income.addEventListener('click',(event)=>{
+     document.querySelector('#modalWindow').style.visibility='visible'
+})
+register.addEventListener('click',()=>{
+    localStorage.setItem('email',modalEmail.value)
+
+    login()
+})
+async function login(){
+  try{
+    const responce=await fetch('https://george.pythonanywhere.com/api/login/',{
+      method:"POST",
+      headers:{
+        "Content-Type": "application/json",
+        
+      },
+      body: JSON.stringify({
+        email:'logIn',
+      })
+    });
+    console.log(responce)
+    const data= await responce.json()
+    console.log(data)
+    
+  } catch (error){
+    alert('Error during login:', error)
+    
+  }
+}
+
+
