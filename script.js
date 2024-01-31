a8e3d3b8e18f9f0a43543d1205c7e5421686a700
lasha <gnomi490@gmail.com>
2024-01-31 11:22:43 +0400

    go to blog

--- script.js
+++ script.js
@@ -1,71 +1,64 @@
-const income=document.querySelector('.income');
-const cross=document.querySelector('.cross')
-const register=document.querySelector('.register')
-const modalEmail=document.querySelector('.modal__email')
+const income = document.querySelector(".income");
+const cross = document.querySelector(".cross");
+const register = document.querySelector(".register");
+const modalEmail = document.querySelector(".modal__email");
 
-
-if(localStorage.getItem('email')){
-  modalEmail.value=localStorage.getItem('email')
+if (localStorage.getItem("email")) {
+  modalEmail.value = localStorage.getItem("email");
 }
 
-
-
-
-cross.addEventListener('click',()=>{
-    document.querySelector('#modalWindow').style.visibility='hidden'
-})
-income.addEventListener('click',(event)=>{
-     document.querySelector('#modalWindow').style.visibility='visible'
-     if(income.innerHTML=='დაამატე ბლოგი'){
-      window.location.href='pages/NewBlog/newBlog.html'
-     }
-})
-register.addEventListener('click',(event)=>{
-  if(event.target.value=='შესვლა'){
-    login()
-    if(localStorage.getItem('token') != ''){
-      register.value='კარგი'
-      document.querySelector('.inc').innerHTML='წარმატებული ავტორიზაცია'
-      modalEmail.style.display='none'
-      document.querySelector('.postp').style.display='none'
-      document.querySelector('.tick').style.display='flex'
-      document.querySelector('.tick').style.marginBottom='20px'
-      
-    } 
-  }else{
-      document.querySelector('#modalWindow').style.display='none'
-      document.querySelector('.income').innerHTML='დაამატე ბლოგი'
-      document.querySelector('.income').style.width='153px'
+cross.addEventListener("click", () => {
+  document.querySelector("#modalWindow").style.visibility = "hidden";
+});
+income.addEventListener("click", (event) => {
+  document.querySelector("#modalWindow").style.visibility = "visible";
+  if (income.innerHTML == "დაამატე ბლოგი") {
+    window.location.href = "pages/NewBlog/newBlog.html";
+  }
+});
+register.addEventListener("click", (event) => {
+  if (event.target.value == "შესვლა") {
+    login();
+    if (localStorage.getItem("token") != "") {
+      register.value = "კარგი";
+      document.querySelector(".inc").innerHTML = "წარმატებული ავტორიზაცია";
+      modalEmail.style.display = "none";
+      document.querySelector(".postp").style.display = "none";
+      document.querySelector(".tick").style.display = "flex";
+      document.querySelector(".tick").style.marginBottom = "20px";
     }
-    
-   
-})
-async function login(){
-  try{
-    const response=await fetch('https://george.pythonanywhere.com/api/login/',{
-      method:"POST",
-      headers:{
-        "Content-Type": "application/json",
-        
-      },
-      body: JSON.stringify({
-        email:modalEmail.value,
-      })
-    });
-    console.log(response.ok)
-    const data= await response.json()
-    localStorage.setItem('token',data.token)
-    console.log(data)
-    
-    
-  } catch (error){
-    alert('Error during login:', error)
-    
+  } else {
+    document.querySelector("#modalWindow").style.display = "none";
+    document.querySelector(".income").innerHTML = "დაამატე ბლოგი";
+    document.querySelector(".income").style.width = "153px";
+  }
+});
+async function login() {
+  try {
+    const response = await fetch(
+      "https://george.pythonanywhere.com/api/login/",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: modalEmail.value,
+        }),
+      }
+    );
+    console.log(response.ok);
+    const data = await response.json();
+    localStorage.setItem("token", data.token);
+    console.log(data);
+  } catch (error) {
+    alert("Error during login:", error);
   }
 }
 
-
-
+document.querySelector(".full-paragraph").addEventListener("click", () => {
+  window.location.href = "pages/Blog/blog.html";
+});
 
 const item = [...document.querySelectorAll(".item")];
 const nav = [...document.querySelectorAll(".nav-list")];
@@ -73,4 +66,3 @@ console.log(nav);
 
 const foo = item.filter((ele, index) => ele[index] == "UI/UX");
 console.log(foo);
-
